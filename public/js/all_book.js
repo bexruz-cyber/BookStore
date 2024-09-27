8081a0e8ac9bd3653284793319e28693a588398c
bexruz-cyber <mayoq2024@gmail.com>
2024-09-27 23:16:57 +0500

    add

--- public/js/all_book.js
+++ public/js/all_book.js
@@ -3,25 +3,23 @@ const book_box_open_toogles = document.querySelectorAll(".book_box_open_toogle")
 
 book_box_open_toogles.forEach((toogle, index) => {
   toogle.addEventListener("click", function() {
-    const books_box = books_boxes[index]; // index orqali har bir toogle ga mos books_box ni olamiz
-    const childrenCount = books_box.children.length; // books_box ichidagi child elementlar sonini aniqlaymiz
-    const maxHeightPerChild = 531; // har bir child element uchun maksimal balandlik
-    const totalHeight = childrenCount * maxHeightPerChild; // umumiy balandlikni hisoblaymiz
+    const books_box = books_boxes[index];
+    const childrenCount = books_box.children.length; 
+    const maxHeightPerChild = 531; 
+    const totalHeight = childrenCount * maxHeightPerChild; 
 
-    // Har bir tugmani bosganingizda, faqat tegishli `books_box` ochilib-yopiladi
     books_boxes.forEach((box, i) => {
       if (i !== index) {
-        box.style.maxHeight = null; // Qolgan `books_box` larni yopamiz
-        box.classList.remove("open"); // Animatsiya sinfini ham olib tashlaymiz
+        box.style.maxHeight = null; 
+        box.classList.remove("open"); 
       }
     });
 
-    // Joriy `books_box` ning holatini o'zgartiramiz
     if (books_box.classList.contains("open")) {
-      books_box.style.maxHeight = null; // Yopamiz
+      books_box.style.maxHeight = null;
       books_box.classList.remove("open");
     } else {
-      books_box.style.maxHeight = totalHeight + "px"; // Ochamiz
+      books_box.style.maxHeight = totalHeight + "px"; 
       books_box.classList.add("open");
     }
   });
@@ -36,16 +34,15 @@ const mobile_books_box_open_toogle = document.querySelectorAll(".mobile_books_bo
 
 mobile_books_box_open_toogle.forEach((toogle, index) => {
   toogle.addEventListener("click", function() {
-    const current_mobile_books_box = mobile_books_box[index]; // index orqali har bir toogle ga mos mobile_books_box ni olamiz
-    const childrenCount = current_mobile_books_box.children.length; // mobile_books_box ichidagi child elementlar sonini aniqlaymiz
-    const maxHeightPerChild = 404; // har bir child element uchun maksimal balandlik
-    const totalHeight = childrenCount * maxHeightPerChild; // umumiy balandlikni hisoblaymiz
+    const current_mobile_books_box = mobile_books_box[index]; 
+    const childrenCount = current_mobile_books_box.children.length;
+    const maxHeightPerChild = 404;
+    const totalHeight = childrenCount * maxHeightPerChild;
 
-    // Har bir tugmani bosganingizda, faqat tegishli `mobile_books_box` ochilib-yopiladi
     mobile_books_box.forEach((box, i) => {
       if (i !== index) {
-        box.style.maxHeight = null; // Qolgan `mobile_books_box` larni yopamiz
-        box.classList.remove("open"); // Animatsiya sinfini ham olib tashlaymiz
+        box.style.maxHeight = null;
+        box.classList.remove("open");
       }
     });
 
